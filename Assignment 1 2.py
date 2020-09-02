3241e45e4a89ab39916d7958cc021f02655c405a
Gaby-K <33060717+Gaby-K@users.noreply.github.com>
2020-09-02 16:10:31 +0800

    Add files via upload

---
+++ Assignment 1 2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#This notebook will be mainly used for my capstone project
+# In[1]:
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
